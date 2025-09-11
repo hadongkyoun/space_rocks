615b86b3ce475423760fbcfc65eae09299506844
hadongkyoun <gkehdrbs36@naver.com>
2025-09-11 22:39:24 +0900

    Add wrap system
    access to physics_state for no glitch
    rb2D not allow to change position.
    
    Screensize will be controled by main scene.

--- Player/player.gd
+++ Player/player.gd
@@ -3,6 +3,9 @@ extends RigidBody2D
 @export var engine_power = 500
 @export var spin_power = 8000
 
+# For screen wrap
+var screensize = Vector2.ZERO
+
 # Strength of engine
 var thrust = Vector2.ZERO
 # Rotate direction of ship
@@ -13,7 +16,8 @@ var state = INIT
 
 func _ready() -> void:
 	change_state(ALIVE)
-
+	screensize = get_viewport_rect().size
+	
 func _process(delta: float) -> void:
 	get_input()
 	
@@ -21,13 +25,19 @@ func _physics_process(delta: float) -> void:
 	constant_force = thrust
 	constant_torque = rotation_dir * spin_power
 
+func _integrate_forces(physics_state: PhysicsDirectBodyState2D) -> void:
+	var xform = physics_state.transform
+	xform.origin.x = wrapf(xform.origin.x, 0, screensize.x)
+	xform.origin.y = wrapf(xform.origin.y, 0, screensize.y)
+	physics_state.transform = xform
+	
 func get_input():
 	thrust = Vector2.ZERO
 	if state in [DEAD, INIT]:
 		return
 	if Input.is_action_pressed("thrust"):
 		thrust = transform.x * engine_power
-	rotation_dir = Input.get_axis("rotate_left", "roate_right")
+	rotation_dir = Input.get_axis("rotate_left", "rotate_right")
 
 func change_state(new_state):
 	match new_state:
